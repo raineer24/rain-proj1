9e1e5e9d388e54e1fd8f4ba22375b2e81cc31bc3
raineer <rain@gmail.com>
2021-09-10 19:23:02 +0800

    sccs material

---
+++ src/app/shared/materialize/materialize.module.ts
@@ -0,0 +1,67 @@
+import { NgModule } from "@angular/core";
+
+import { MatInputModule } from "@angular/material/input";
+import { MatTableModule } from "@angular/material/table";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatSelectModule } from "@angular/material/select";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatTableModule,
+    MatProgressBarModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatDividerModule,
+    MatCheckboxModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatTableModule,
+    MatProgressBarModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatDividerModule,
+    MatCheckboxModule,
+  ],
+})
+export class MaterialModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { MaterialModule } from "./materialize/materialize.module";
+import { CommonModule } from "@angular/common";
+@NgModule({
+  declarations: [],
+  imports: [MaterialModule, CommonModule],
+  exports: [],
+})
+export class SharedModule {}
