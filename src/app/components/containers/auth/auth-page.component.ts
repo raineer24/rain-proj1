b7318f280239d2b12e1c626f519514a54ec6cc9a
raineer <rain@gmail.com>
2021-09-10 14:26:19 +0800

    auth page component

--- src/app/components/containers/auth/auth-page.component.ts
+++ src/app/components/containers/auth/auth-page.component.ts
@@ -1,2 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-auth-page",
+  templateUrl: "./auth-page.component.html",
+  styleUrls: ["./auth-page.component.scss"],
+})
+export class AuthPageComponent implements OnInit {
+  ngOnInit(): void {}
+}
