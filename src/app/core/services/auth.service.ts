250ea4fdbd638e76559f6c836c70d040b8ca7994
raineer <rain@gmail.com>
2021-09-11 20:32:11 +0800

    action payload effects

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { UserDetailsModel } from "../models/users/user-details.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {}
