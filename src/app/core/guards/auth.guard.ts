c1f036c5cba86e4ddd94614187463c662e9c1cf5
raineer <rain@gmail.com>
2021-09-12 11:45:41 +0800

    auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { select, Store } from "@ngrx/store";
