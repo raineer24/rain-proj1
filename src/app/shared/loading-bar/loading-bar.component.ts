d17d64a115610d7939da91d3c66df647a39a2523
raineer <rain@gmail.com>
2021-09-14 16:06:20 +0800

    app loading bar

---
+++ src/app/shared/loading-bar/loading-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-loading-bar",
+  templateUrl: "./loading-bar.component.html",
+  styleUrls: [],
+})
+export class LoadingBarComponent {}
