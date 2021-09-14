daf4b290629c7cd83a26b70e2526e653d5beb403
raineer <rain@gmail.com>
2021-09-14 17:47:07 +0800

    angular material progress bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { MaterialModule } from "./materialize/materialize.module";
 import { CommonModule } from "@angular/common";
+import { LoadingBarComponent } from "./loading-bar/loading-bar.component";
 @NgModule({
-  declarations: [],
+  declarations: [LoadingBarComponent],
   imports: [MaterialModule, CommonModule],
-  exports: [],
+  exports: [LoadingBarComponent],
 })
 export class SharedModule {}
