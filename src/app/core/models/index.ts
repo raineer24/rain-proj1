cedd098670a697d1f0b503d2e1648580698f0891
raineer <rain@gmail.com>
2021-09-15 13:01:50 +0800

    auth service ts

---
+++ src/app/core/models/index.ts
@@ -0,0 +1,2 @@
+export * from "./users/user-credentials.model";
+export * from "./users/user-details.model";
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { UserDetailsModel } from "../models/users/user-details.model";
+import { UserDetailsModel, UserCredentialsModel } from "../models";
 import { environment } from "../../../environments/environment";
 @Injectable({
   providedIn: "root",
 })
-export class AuthService {}
+export class AuthService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+    }),
+  };
+  constructor(private http: HttpClient) {}
+  login(credentials: UserCredentialsModel): Observable<{}> {
+    return this.http.post("api/login", credentials, this.httpOptions);
+  }
+}
