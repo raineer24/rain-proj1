d24a101e7de51c073c23f4c19b71ee737b11547d
raineer <rain@gmail.com>
2021-09-25 10:56:37 +0800

    register page

---
+++ src/app/components/containers/auth/register-page/register-page.component..ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AppState } from "../../../../store/app.state";
+import { Store } from "@ngrx/store";
+import { RegisterUser } from "../../../../store/actions/auth.actions";
+import { UserCredentialsModel } from "../../../../core/models/users/user-credentials.model";
+
+@Component({
+  selector: "app-register-page",
+  templateUrl: "./register-page.component.html",
+  styleUrls: ["./register-page.component.scss"],
+})
+export class RegisterPageComponent implements OnInit {}
