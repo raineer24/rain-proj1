a348d01db50e7cf1daeb26388326124e2868ba06
raineer <rain@gmail.com>
2021-09-28 14:17:59 +0800

    event emitter save button click

--- src/app/components/containers/auth/register-page/register-page.component.ts
+++ src/app/components/containers/auth/register-page/register-page.component.ts
@@ -19,41 +19,14 @@ export class RegisterPageComponent implements OnInit {
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
   fd = new FormData();
   constructor(private fb: FormBuilder, private store: Store<AppState>) {}
-
+  formCheck: any = "";
   form: FormGroup;
-  ngOnInit(): void {
-    this.initForm();
-  }
 
-  initForm() {
-    this.form = this.fb.group({
-      email: [
-        "",
-        Validators.compose([
-          Validators.required,
-          Validators.pattern(this.emailPattern),
-        ]),
-      ],
-      password: [
-        "",
-        Validators.compose([Validators.required, Validators.minLength(6)]),
-      ],
-      username: ["", Validators.required],
-      first_name: ["", Validators.required],
-      image: [null, Validators.required],
-      password2: [null, Validators.required],
-    });
-  }
+  ngOnInit(): void {}
 
   onRegister(event) {
-    console.log("this.form.value", this.form.value);
-    this.fd.append("image", this.form.get("image").value);
-    this.fd.append("email", this.form.value.email);
-    this.fd.append("password", this.form.value.password);
-    this.fd.append("username", this.form.value.username);
-    this.fd.append("first_name", this.form.value.first_name);
-    this.fd.append("password2", this.form.value.password2);
-
-    this.store.dispatch(new RegisterUser(this.fd));
+    // this.formCheck = event;
+    //console.log(event, this.formCheck["controls"]);
+    console.log(event);
   }
 }
--- src/app/components/presentational/auth/register-form/register-form.component.ts
+++ src/app/components/presentational/auth/register-form/register-form.component.ts
@@ -1,12 +1,52 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { AppState } from "../../../../store/app.state";
+import { Store } from "@ngrx/store";
 
 @Component({
   selector: "app-register-form",
   templateUrl: "./register-form.component.html",
   styleUrls: ["./register-form.component.scss"],
 })
-export class RegisterFormComponent {
-  @Input() form: FormGroup;
+export class RegisterFormComponent implements OnInit {
+  public form: FormGroup;
   @Output() register: EventEmitter<void> = new EventEmitter<void>();
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
+  constructor(private fb: FormBuilder, private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = this.fb.group({
+      email: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.emailPattern),
+        ]),
+      ],
+      password: [
+        "",
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
+      username: ["", Validators.required],
+      first_name: ["", Validators.required],
+      image: [null, Validators.required],
+      password2: [null, Validators.required],
+    });
+  }
+
+  save() {
+    //  this.isSubmited = true;
+    if (this.form.valid) {
+      this.register.emit(this.form.value);
+    }
+  }
 }
