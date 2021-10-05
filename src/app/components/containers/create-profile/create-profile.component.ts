0f11bf82dd518bd44bba028ae405aafd349dd8e9
raineer <rain@gmail.com>
2021-10-05 15:44:29 +0800

    user profile patch value

--- src/app/components/containers/create-profile/create-profile.component.ts
+++ src/app/components/containers/create-profile/create-profile.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
 import { Store, select, ActionsSubject } from "@ngrx/store";
 import { Subscription, Observable, of, Subject } from "rxjs";
 import { MatIconRegistry } from "@angular/material/icon";
+import { GetUserAction } from "../../../store/actions/auth.actions";
+import * as AuthActions from "../../../store/actions/auth.actions";
 
 import {
   skipWhile,
@@ -15,7 +17,11 @@ import { DomSanitizer } from "@angular/platform-browser";
 import { Router, ActivatedRoute } from "@angular/router";
 import { ofType } from "@ngrx/effects";
 import { AppState } from "../../../store/app.state";
-import { selectAuthUserId } from "../../../store/reducers/auth.reducer";
+import {
+  selectAuthUserId,
+  selectAuthUser,
+  selectAuthUserProfile,
+} from "../../../store/reducers/auth.reducer";
 import {
   FormBuilder,
   FormGroup,
@@ -33,12 +39,15 @@ import { UserFetch } from "src/app/core/models";
   styleUrls: ["./create-profile.component.scss"],
 })
 export class CreateProfileComponent implements OnInit {
+  profile$: UserFetch;
   isAddMode: boolean;
   id: string;
+  dataId: string;
   profForm: FormGroup;
   @Input() disableForm: boolean;
   dropdownSelected: string;
   selectedStatus: String = "";
+  destroyed$ = new Subject<boolean>();
 
   show = false;
   constructor(
@@ -46,7 +55,8 @@ export class CreateProfileComponent implements OnInit {
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
     private iconRegistry: MatIconRegistry,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private actionsSubj: ActionsSubject
   ) {
     iconRegistry.addSvgIcon(
       "thumbs-up",
@@ -82,15 +92,51 @@ export class CreateProfileComponent implements OnInit {
       facebook_handle: [""],
       youtube_handle: [""],
       twitter_handle: [""],
-      //id: data,
+      id: this.route.snapshot.params["id"],
     });
     this.store.pipe(select(selectAuthUserId), take(1)).subscribe((data) => {
       console.log("data", data);
-      // this.id = data;
-      //this.id = data["id"];
-      //  console.log("this id", this.id);
-      // this.store.dispatch(new GetUserAction({ id: this.id }));
+      this.dataId = data;
+
+      this.store.dispatch(new GetUserAction({ id: this.dataId }));
     });
+
+    if (!this.isAddMode) {
+      this.actionsSubj
+        .pipe(
+          ofType(AuthActions.AuthActionsTypes.GET_USER_SUCCESS),
+          takeUntil(this.destroyed$)
+        )
+        .subscribe((data: any) => {
+          console.log("xdatas", data);
+          console.log("xdata", data["payload"]["user_profile"][0]);
+          this.profile$ = data["payload"]["user_profile"][0];
+          console.log("prof", this.profile$.job_location);
+          let skills = data["payload"]["user_skill"][0].skills[0];
+
+          if (data) {
+            this.profForm.patchValue({
+              company_name: this.profile$.company_name,
+              website: this.profile$.website,
+              job_location: this.profile$.job_location,
+              status: this.profile$.status,
+              bio: this.profile$.bio,
+              areas_of_expertise: skills,
+              youtube_handle: this.profile$.youtube_handle,
+              twitter_handle: this.profile$.twitter_handle,
+              instagram_handle: this.profile$.instagram_handle,
+              facebook_handle: this.profile$.facebook_handle,
+              id: this.profile$.id,
+            });
+          }
+
+          /* hooray, success, show notification alert etc.. */
+        });
+    }
+  }
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
   get status() {
     return this.profForm.get("status");
