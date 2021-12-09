225638c8ffd08bdf7c93af090ef1f364551d8aa9
raineer <rain@gmail.com>
2021-12-09 18:48:32 +0800

    create exp profile

--- src/app/components/containers/add-exp/add-exp.component.ts
+++ src/app/components/containers/add-exp/add-exp.component.ts
@@ -35,10 +35,22 @@ const moment = _rollupMoment || _moment;
   selector: "app-add-exp",
   templateUrl: "./add-exp.component.html",
   styleUrls: ["./add-exp.component.scss"],
+  providers: [
+    DatePipe,
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  ],
 })
 export class AddExperienceComponent implements OnInit {
   formGroup: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private datePipe: DatePipe) {}
 
   ngOnInit() {
     this.formGroup = this.formBuilder.group({
@@ -50,5 +62,24 @@ export class AddExperienceComponent implements OnInit {
       description: [null, Validators.required],
       current: [false, []],
     });
+  }
+
+  get date() {
+    return this.formGroup.get("date");
+  }
+  dateValue() {
+    console.log(this.datePipe.transform(this.date.value, "dd-MM-yyyy"));
+  }
+  toggleCtrState() {
+    const ctrl = this.formGroup.get("end_date");
+    if (ctrl.disabled) {
+      ctrl.enable();
+    } else {
+      ctrl.disable();
+    }
+  }
+
+  onSubmit() {
+    const data = this.formGroup.value;
   }
 }
