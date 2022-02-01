dbccf44821d7fbd5ea632cebe45309a59a507765
wtf <mura man kag wtf>
2022-02-01 11:21:54 +0800

    user action

--- src/app/store/actions/user.actions.ts
+++ src/app/store/actions/user.actions.ts
@@ -11,56 +11,16 @@ import { createAction, props } from "@ngrx/store";
 import { AnyFn } from "@ngrx/store/src/selector";
 
 export enum UserActionTypes {
-  LOAD_USERS = "[User] Load Users",
-  SUCCESS_INIT_USERS = "[userList] Success Init User",
-}
-export class SuccessInitUsers implements Action {
-  readonly type = UserActionTypes.SUCCESS_INIT_USERS;
-  constructor(public payload: any) {}
+  GetUsers = "[Users List] Get users",
+  GetUsersSuccess = "[Users List] Get users success",
 }
 
-export class LoadUsers implements Action {
-  readonly type = UserActionTypes.LOAD_USERS;
-  //constructor(public payload: any) {}
+export class GetUsers implements Action {
+  public readonly type = UserActionTypes.GetUsers;
+}
+export class GetUsersSuccess implements Action {
+  public readonly type = UserActionTypes.GetUsersSuccess;
+  constructor(public payload: UserCredentialsModel[]) {}
 }
 
-export type UserActions = LoadUsers | SuccessInitUsers;
-
-// export const loadUsers = createAction("[Users Management] Load Users", () => ({
-//   showLoader: true,
-// }));
-
-// export const loadUsersSuccess = createAction(
-//   "[Users Management] Load Users Success",
-//   (users: UserCredentialsModel[]) => ({
-//     users,
-//     triggerAction: loadUsers.type,
-//   })
-// );
-
-// export const loadUsersFail = createAction(
-//   "[Users Management] Load Users Fail",
-//   (error: any) => ({
-//     error,
-//     triggerAction: loadUsers.type,
-//   })
-// );
-
-export const getUser = createAction(
-  "GET USER",
-  props<{
-    id: string;
-  }>()
-);
-
-export const getUserSuccess = createAction(
-  "[Auth] Get Current User Success",
-  props<{ payload: any }>()
-);
-
-export const loadUsers = createAction("[Users] Load Users");
-
-export const loadUsersSuccess = createAction(
-  "[Users] load Users Success",
-  props<{ users: any }>()
-);
+export type UserActions = GetUsers | GetUsersSuccess;
