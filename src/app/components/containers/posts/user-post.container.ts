94e8e6d2d814f102ed8ef2392f6155d783ef61fa
wtf <mura man kag wtf>
2022-02-05 12:59:07 +0800

    user post container ts

---
+++ src/app/components/containers/posts/user-post.container.ts
@@ -0,0 +1,4 @@
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Store, select, ActionsSubject } from "@ngrx/store";
+import { Subscription, Observable, of, Subject } from "rxjs";
