78b1b3c0d3c7408bf4e010bfd085f0e8a96a612c
wtf <mura man kag wtf>
2022-02-05 13:09:12 +0800

    state app state reducer

--- src/app/components/containers/posts/user-post.container.ts
+++ src/app/components/containers/posts/user-post.container.ts
@@ -2,3 +2,19 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Store, select, ActionsSubject } from "@ngrx/store";
 import { Subscription, Observable, of, Subject } from "rxjs";
+import { AppState } from "../../../store/app.reducers";
+@Component({
+  selector: "app-user-post",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <div
+     test
+    </div>
+  `,
+  styles: [``],
+})
+export class UserPostComponent implements OnInit {
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {}
+
+  ngOnInit() {}
+}
