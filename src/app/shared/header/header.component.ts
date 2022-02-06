5c21df9befaecde8eb146c0742344f8284417f4d
wtf <mura man kag wtf>
2022-02-06 12:22:30 +0800

    mat icons header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,28 +3,29 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: "app-header",
   styleUrls: ["header.component.scss"],
-  template: ` <mat-toolbar color="primary">
-    <mat-toolbar-row class="mat-toolbar-row-top" fxLayout>
-      <div fxFlex fxLayout fxLayoutAlign="flex-end">
-        <ul fxLayout class="navigation-items">
-          <li>
-            <a routerLink="/login">Log in</a>
-          </li>
-          <li>
-            <a routerLink="/dev">Users</a>
-          </li>
-          <li>
-            <a routerLink="/posts">Blogs</a>
-          </li>
-          <li *ngIf="isSigningUpEnabled">
-            <a class="border" routerLink="/signing-up/organization/init"
-              >Sign up</a
-            >
-          </li>
-        </ul>
-      </div>
-    </mat-toolbar-row>
-  </mat-toolbar>`,
+  template: ` <div
+    class="container"
+    fxLayout="column"
+    fxLayout.xs="column"
+    fxLayoutAlign="center"
+    fxLayoutGap="0px"
+    fxLayoutGap.xs="0"
+  >
+    <div class="item header-content" fxFlex="100%" fxFlexOffset.xs="0">
+      <mat-toolbar>
+        <span mat-card-avatar class="example-header-image"></span>
+        <span>Social Media Management</span>
+        <span class="example-spacer"></span>
+        <button mat-button routerLink="/"><mat-icon>home</mat-icon></button>
+        <button mat-button routerLink="/posts">
+          <span class="material-icons">post_add</span>
+        </button>
+        <button mat-button routerLink="/dev">
+          <span class="material-icons">people</span>
+        </button>
+      </mat-toolbar>
+    </div>
+  </div>`,
 })
 export class HeaderComponent {
   @Input() isSigningUpEnabled: boolean;
