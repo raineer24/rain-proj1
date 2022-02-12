afa92594d2afb3b4f88918f97320bd0769b1566b
wtf <mura man kag wtf>
2022-02-12 10:44:04 +0800

    get post access actions

--- src/app/store/actions/post.actions.ts
+++ src/app/store/actions/post.actions.ts
@@ -5,6 +5,10 @@ export const CREATE_POST = "[CREATE POST PAGE] CREATE_POST";
 export const CREATE_POST_SUCCESS = "[CREATE POST PAGE] CREATE_POST_SUCCESS";
 export const CREATE_POST_FAILED = "[CREATE POST PAGE] CREATE_POST_FAILED";
 
+export const GET_POST = "[ POST ]";
+export const GET_POST_SUCCESS = "GET_POST_SUCCESS";
+export const GET_POST_FAILED = "GET_POST_FAILED";
+
 export const createPost = createAction(
   CREATE_POST,
   props<{ post: FormData }>()
@@ -13,6 +17,16 @@ export const createPost = createAction(
 export const createPostSuccess = createAction(
   CREATE_POST_SUCCESS,
   props<{ post: any }>()
+);
+
+export const getSinglePost = createAction(
+  GET_POST,
+  props<{ postId: string }>()
+);
+
+export const getSinglePostSuccess = createAction(
+  GET_POST_SUCCESS,
+  props<{ post: Posts }>()
 );
 
 export const createPostFailed = createAction(CREATE_POST_FAILED);
