e9b75f2aa1d087b68aa805d1a38425cd3164d094
wtf <mura man kag wtf>
2022-02-13 19:01:41 +0800

    ngFor not working , getting all Posts

--- src/app/components/containers/posts/post-list/posts-list.component.ts
+++ src/app/components/containers/posts/post-list/posts-list.component.ts
@@ -26,14 +26,15 @@ import {
   styleUrls: ["./posts-list.component.scss"],
 })
 export class PostsListComponent implements OnInit, OnDestroy {
-  // posts$: Posts[];
+  posts: Posts[];
+  datus: any;
   $postAppsSubscription: Observable<string>;
   public posts$: Observable<Posts[]> = this.store.pipe(
     select(generateAllPosts)
   );
   constructor(private store: Store<AppState>, private route: ActivatedRoute) {}
   ngOnInit() {
-    // this.store.dispatch(getAllPosts());
+    this.store.dispatch(getAllPosts());
     // this.store.pipe(select(generateAllPosts), take(1)).subscribe((data) => {
     //   console.log("data", data);
     //   //this.posts$ = data;
@@ -41,7 +42,7 @@ export class PostsListComponent implements OnInit, OnDestroy {
 
     this.store.dispatch(PostActions.opened());
     this.posts$.subscribe((data) => {
-      console.log("data", data);
+      this.posts = data;
     });
 
     // this.store
